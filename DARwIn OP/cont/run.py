9cce184b1c1d2c637842e8ecb4083502e9d8a43c
99angelrm <99angelrm@gmail.com>
2021-07-14 18:10:54 -0500

    Fue modificado el script
    La carpeta ahora es ingresada por medio de la terminal que se desplega cuando se corre un archivo de python.

--- DARwIn OP/cont/run.py
+++ DARwIn OP/cont/run.py
@@ -3,7 +3,7 @@ import fileinput
 import shutil
 import pathlib
 
-dir=r"C:\Users\ANGEL\Documents\Verano 2021\Soccer\controllers\rotar3"
+dir=input("Ingresa la carpeta del controlador a: ")
 base=str(pathlib.Path(__file__).parent.resolve())+r"\controladorBASE"
 nombre=dir.split("\\")[-1]
 for file in os.listdir(base):
